9b4ed5ded770f77dfc5706050770316c3216bb08
erichaofun <164448257+erichaofun@users.noreply.github.com>
2024-05-08 14:00:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -29,27 +29,23 @@ var random2 = Math.floor(Math.random() * 6);
 player2_dicenum=random2+1
 
 
-
 var image2 = document.getElementById('img2');
 var imagepath2="./images/dice" + player2_dicenum + ".png";
 image2.setAttribute('src', imagepath2);
 
 
 if (player1_dicenum>player2_dicenum){
-    var list=document.firstElementChild.lastElementChild.firstElementChild.firstElementChild;
-    list.innerHTML=" &#128681 Player 1 Wins! ";
+    document.querySelector("h1").innerHTML=" &#128681 Player 1 Wins! ";
 
 }
 
 else if(player1_dicenum<player2_dicenum){
 
-    var list=document.firstElementChild.lastElementChild.firstElementChild.firstElementChild;
-    list.innerHTML="Player 2 Wins! &#128681 ";
+    document.querySelector("h1").innerHTML="Player 2 Wins! &#128681 ";
 }
 
 else{
-    var list=document.firstElementChild.lastElementChild.firstElementChild.firstElementChild;
-    list.innerHTML=" Draw !!!  ";
+    document.querySelector("h1").innerHTML=" Draw !!!  ";
 }
 
 
@@ -58,8 +54,7 @@ else{
 
     
 function refresh(){
-    var list=document.firstElementChild.lastElementChild.firstElementChild.firstElementChild;
-    list.innerHTML=" Roll the dice again ";
+    document.querySelector("h1").innerHTML=" Roll the dice again ";
 
     var image = document.getElementById('img1');
     var imagepath="./images/dice-ques.png";
